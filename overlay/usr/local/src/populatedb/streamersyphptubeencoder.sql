d4ae9ef646c9597acafaad510ff2c69fb89df89e
MPTMG <ptm@tapbop.com>
2018-10-17 21:23:49 -0400

    Add Encoder
    Adding YouPHPTube-Encoder For Processing Recordings

---
+++ overlay/usr/local/src/populatedb/streamersyphptubeencoder.sql
@@ -0,0 +1,5 @@
+SET NAMES utf8mb4;
+
+INSERT INTO `streamers` (`id`, `siteURL`, `user`, `pass`, `priority`, `isAdmin`, `created`, `modified`) VALUES
+(1,	'replacedatfirstboot',	'admin',	'plaintextpass',	1,	1,	'2018-10-17 19:54:22',	'2018-10-17 19:54:22'),
+(2,	'replacedatfirstboot',	'admin',	'hashpass',	1,	0,	'2018-10-17 19:54:27',	'2018-10-18 00:17:10');
